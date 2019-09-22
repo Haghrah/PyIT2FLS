169572d76ba7ab4c39bf3581d6eb1595e78ea64d
Amir Arslan Haghrah <7789561+Haghrah@users.noreply.github.com>
2019-09-22 20:55:19 +0330

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyit2fls',
+      version='0.1',
+      description='Interval Type 2 Fuzzy Logic Systems in Python',
+      url='https://github.com/Haghrah/PyIT2FLS',
+      author='Amir Arslan Haghrah',
+      author_email='arslan.haghrah@gmail.com',
+      license='GPL-3.0',
+      packages=['pyit2fls'],
+      install_requires=['numpy', 'matplotlib', ],
+      zip_safe=False)
