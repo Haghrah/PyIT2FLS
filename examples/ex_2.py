bbf058dd014298fda32aeaecdaacc0f6f0a564a4
Amir Arslan Haghrah <7789561+Haghrah@users.noreply.github.com>
2020-10-31 13:59:41 +0330

    Update ex_2.py

--- examples/ex_2.py
+++ examples/ex_2.py
@@ -18,36 +18,36 @@ from numpy import linspace
 domain = linspace(0., 1., 100)
 
 # The first IT2FLS is defined as a Gaussian set with uncertain mean value.
-# The mean center = 0.25, 
-# the mean spread = 0.1, 
-# the standard deviation = 0.1, and 
-# the height = 1.
+# Center of the mean = 0.25, 
+# Spread of the mean = 0.1, 
+# Standard deviation = 0.1, and 
+# Height = 1.
 A = IT2FS_Gaussian_UncertMean(domain, [0.25, 0.1, 0.1, 1.])
 
 # The second IT2FLS is defined as a Gaussian set with uncertain mean value.
-# The mean center = 0.5, 
-# the mean spread = 0.1, 
-# the standard deviation = 0.1, and 
-# the height = 1.
+# Center of the mean = 0.5, 
+# Spread of the mean = 0.1, 
+# Standard deviation = 0.1, and 
+# Height = 1.
 B = IT2FS_Gaussian_UncertMean(domain, [0.5, 0.1, 0.1, 1.])
 
 # The third IT2FLS is defined as a Gaussian set with uncertain mean value.
-# The mean center = 0.75, 
-# the mean spread = 0.1, 
-# the standard deviation = 0.1, and 
-# the height = 1.
+# Center of the mean = 0.75, 
+# Spread of the mean = 0.1, 
+# Standard deviation = 0.1, and 
+# Height = 1.
 C = IT2FS_Gaussian_UncertMean(domain, [0.75, 0.1, 0.1, 1.])
 
-# All the three sets are plotted in same figure with legends and the ouput is saved 
-# to a file.
+# All the three sets are plotted in the same figure with legends and the ouput is saved 
+# to a file named multiSet.pdf.
 IT2FS_plot(A, B, C, title="", legends=["Small","Medium","Large"], filename="multiSet")
 
 # The meet of two sets A and B is calculated as the set AB, and plotted. 
-# The T-norm used in meet operation is minimum T-norm.
+# T-norm used in the meet operation is minimum T-norm.
 AB = meet(domain, A, B, min_t_norm)
 AB.plot(filename="meet")
 
 # The join of two sets B and C is calculated as the set BC, and plotted. 
-# The S-norm used in join operation is maximum S-norm.
+# S-norm used in the join operation is maximum S-norm.
 BC = join(domain, B, C, max_s_norm)
 BC.plot(filename="join")
