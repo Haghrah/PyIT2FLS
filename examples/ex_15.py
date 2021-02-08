36c4e37ae489988acf4d233f5b30124655362019
haghrah <arslan.haghrah@gmail.com>
2021-02-08 15:49:16 +0330

    Some comments ...

--- examples/ex_15.py
+++ examples/ex_15.py
@@ -15,10 +15,19 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from numpy import linspace, meshgrid, zeros
 from time import time
 
+#%%
+# The universe of discourse for all input variables are same -> [0, 1)
 domain = linspace(0., 1., 100)
 
+
+#%%
+# The fuzzy system will be evaluated in the [0, 1) x [0, 1) space. 
 X1, X2 = meshgrid(domain, domain)
 
+
+# %%
+# Eight functions are defined, that are used in consequence
+# part of the rules in the fuzzy rule-base.
 def y11(x1, x2):
     return x1 + x2 + 1.
 
@@ -43,6 +52,10 @@ def y41(x1, x2):
 def y42(x1, x2):
     return -0.5 * x1 + x2 - 0.5
 
+
+# %%
+# Defined surfaces for being used in consequence part of rules are 
+# evaluated and plotted here:
 Y11 = y11(X1, X2)
 Y12 = y12(X1, X2)
 Y21 = y21(X1, X2)
@@ -83,19 +96,26 @@ surf = ax.plot_surface(X1, X2, Y42, cmap=cm.coolwarm,
 plt.show()
 
 
+# %%
+# The fuzzy sets representing the universe of discourse of the 
+# fuzzy system are defined and plotted here:
 Small = T1FS(domain, gaussian_mf, [0, 0.15, 1.])
 Big = T1FS(domain, gaussian_mf, [1., 0.15, 1.])
 
 T1FS_plot(Small, Big, title="Sets", 
           legends=["Small", "Big"])
 
+# The fuzzy system is created using the T1TSK class:
 SYS = T1TSK()
 
+# Input and output variables are defined and added to the fuzzy system.
 SYS.add_input_variable("x1")
 SYS.add_input_variable("x2")
 SYS.add_output_variable("y1")
 SYS.add_output_variable("y2")
 
+# Rule-base of the fuzzy system is defined here:
+# The previously defined functions (yij) are used here.
 SYS.add_rule([("x1", Small), ("x2", Small)], 
              [("y1", y11), 
               ("y2", y12)])
@@ -110,11 +130,15 @@ SYS.add_rule([("x1", Big), ("x2", Big)],
               ("y2", y42)])
 
 
+# %%
+# The output planes of the fuzzy system are evaluated and plotted here:
 Z1 = zeros(shape=X1.shape)
 Z2 = zeros(shape=X1.shape)
 
 for i, x1 in zip(range(len(domain)), domain):
     for j, x2 in zip(range(len(domain)), domain):
+        # The params input of the evaluate function indicates the inputs of
+        # the functions given in the consequence part of the rules.
         z = SYS.evaluate({"x1":x1, "x2":x2}, params=(x1, x2))
         Z1[i, j], Z2[i, j] = z["y1"], z["y2"]
 
