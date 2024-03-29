daf3816843eb804dc5e10fc09d4d8e094b35a2a2
Amir Arslan Haghrah <7789561+Haghrah@users.noreply.github.com>
2024-03-30 01:52:56 +0330

    Update learning.py

--- pyit2fls/learning.py
+++ pyit2fls/learning.py
@@ -19,6 +19,48 @@ class T1TSK_ML_Model:
         self.M = TSKM
         self.mf = mf
         self.c = c
+    
+    def d0(self, d0x):
+        s = 1.
+        d = 0.
+        n = 0.
+        for l in range(self.M):
+            s = 1.
+            for i in range(self.N):
+                s *= self.mf[l][i].d0(d0x[i], self.p[l][i][0], self.p[l][i][1])
+            n += self.q[l] * s
+            d += s
+        return self.c * n / d
+    
+    def d1(self, d0x, d1x):
+        s1 = 0.
+        s2 = 0.
+        s3 = 0.
+        s4 = 0.
+        
+        for l in range(self.M):
+            s5 = 1.
+            for i in range(self.N):
+                s5 *= self.mf[l][i].d0(d0x[i], self.p[l][i][0], self.p[l][i][1])
+            s1 += self.q[l] * s5
+            s2 += s5
+        
+        for j in range(self.N):
+            s7 = 0.
+            s8 = 0.
+            for l in range(self.M):
+                s6 = 1.
+                for i in range(self.N):
+                    if i != j:
+                        s6 *= self.mf[l][i].d0(d0x[i], self.p[l][i][0], self.p[l][i][1])
+                
+                s7 += self.q[l] * self.mf[l][j].d1(d0x[j], self.p[l][j][0], self.p[l][j][1]) * s6
+                s8 += self.mf[l][j].d1(d0x[j], self.p[l][j][0], self.p[l][j][1]) * s6
+            
+            s3 += d1x[j] * s7
+            s4 += d1x[j] * s8
+        
+        return (s3 * s2 - s1 * s4) / s2 ** 2
 
 
 class T1TSK_ML:
