411cc034b39ba9e79ca8f0ee21c5b9ea602275d8
Amir Arslan Haghrah <7789561+Haghrah@users.noreply.github.com>
2024-04-03 02:54:27 +0330

    T1TSK-based machine learning model ...

--- pyit2fls/learning.py
+++ pyit2fls/learning.py
@@ -6,8 +6,18 @@ Created on Sat Mar 30 01:39:58 2024
 @author: arslan
 """
 
-from numpy import (reshape, )
-from .pyit2fls import (gaussian_mf, )
+from numpy import (reshape, exp, array, zeros_like, )
+from numpy.linalg import (norm, )
+from numpy.random import (rand, )
+from scipy.optimize import (differential_evolution, minimize, )
+
+class gaussian_mf_learning:
+
+    def d0(self, x, c, v):
+        return exp(- ((x - c) / v) ** 2)
+
+    def d1(self, x, c, v):
+        return - 2 * ((x - c) / v ** 2) * exp(- ((x - c) / v) ** 2)
 
 
 class T1TSK_ML_Model:
@@ -71,12 +81,53 @@ class T1TSK_ML_Model:
 
 class T1TSK_ML:
 
-    def __init__(self, ):
-        pass
+    def __init__(self, TSKN, TSKM, Bounds=None, algorithm="DE"):
+        self.TSKN = TSKN
+        self.TSKM = TSKM
+        self.algorithm = algorithm
+        self.paramNum = TSKM * (2 * TSKN + 1)
+        self.params = rand(self.paramNum, )
+        self.Bounds = [Bounds, ] * self.paramNum
+        self.mf = gaussian_mf_learning()
+        self.model = T1TSK_ML_Model(self.params, self.TSKN, self.TSKM, 
+                                    [[self.mf, ] * self.TSKN, ] * self.TSKM)
 
+    def error(self, P, X, y):
+        model = T1TSK_ML_Model(P, self.TSKN, self.TSKM, 
+                               [[self.mf, ] * self.TSKN, ] * self.TSKM)
+        o = zeros_like(y)
+        for i, x in zip(range(len(y)), X):
+            o[i] = model.d0(x)
+        return norm(y - o)
 
     def fit(self, X, y):
-        pass
+        if self.algorithm == "DE":
+            result = differential_evolution(self.error, bounds=self.Bounds, 
+                                            args=(X, y), disp=True)
+        elif self.algorithm == "Nelder-Mead":
+            result = minimize(self.error, self.params, args=(X, y), 
+                              method=self.algorithm, bounds=self.Bounds, 
+                              options={"disp":True, })
+        elif self.algorithm == "Powell":
+            result = minimize(self.error, self.params, args=(X, y), 
+                              method=self.algorithm, bounds=self.Bounds, 
+                              options={"disp":True, })
+        elif self.algorithm == "CG":
+            result = minimize(self.error, self.params, args=(X, y), 
+                              method=self.algorithm, 
+                              options={"disp":True, })
+        else:
+            raise ValueError(self.algorithm + " algorithm is not supported!")
+        self.params = result.x
+        self.model = T1TSK_ML_Model(self.params, self.TSKN, self.TSKM, 
+                                    [[self.mf, ] * self.TSKN, ] * self.TSKM)
+        return self.error(self.params, X, y)
+
+    def score(self, X):
+        o = []
+        for x in X:
+            o.append(self.model.d0(x))
+        return array(o)
 
 
 
