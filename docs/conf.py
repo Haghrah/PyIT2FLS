0947b212336728194323344b056dae2c86c2264a
haghrah <arslan.haghrah@gmail.com>
2024-11-24 18:55:55 +0330

    ...

--- docs/conf.py
+++ docs/conf.py
@@ -14,17 +14,12 @@ release = '0.8.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_material'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
@@ -34,7 +29,7 @@ extensions = ['sphinx.ext.autodoc', ]
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../pyit2fls'))
 
 html_theme_options = {
     # If False, expand all TOC entries
--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,3 @@
-.. PyIT2FLS documentation master file, created by
-   sphinx-quickstart on Thu Apr  4 01:46:51 2024.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to PyIT2FLS's documentation!
 ====================================
 
