9ed95ff05923f981536c42e97a2c5f8918159458
haghrah <arslan.haghrah@gmail.com>
2024-12-22 11:26:50 +0330

    Some docstrings for fuzzy matrices ...

--- pyit2fls/fuzzymatrix.py
+++ pyit2fls/fuzzymatrix.py
@@ -11,22 +11,166 @@ from numpy import (array, zeros, min, max, sum,
                    array_equal, )
 
 def T1FMatrix(matrix, t1fs):
+    """
+    Creating a type 1 fuzzy matrix.
+    
+    .. rubric:: Parameters
+    
+    matrix : numpy (n, m, ) shaped array
+        
+        The input matrix.
+    
+    t1fs : T1FS 
+        
+        Type 1 fuzzy set describing the matrix elements.
+    
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns membership degrees associated with the elements of the input matrix.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    """
     return t1fs(matrix)
 
 def T1FMatrix_Complement(matrix):
+    """
+    Calculating complement of a type 1 fuzzy matrix.
+    
+    .. rubric:: Parameters
+    
+    matrix : numpy (n, m, ) shaped array
+        
+        The input matrix.
+    
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns complement of the input matrix.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    >>> t1fmatrix2 = T1FMatrix_Complement(t1fmatrix1)
+    """
     return 1 - matrix
 
 def T1FMatrix_Intersection(m1, m2, t_norm):
+    """
+    Calculating intersection of two type 1 fuzzy matrices.
+    
+    .. rubric:: Parameters
+    
+    m1 : numpy (n, m, ) shaped array
+        
+        The first fuzzy matrix.
+    
+    m2 : numpy (n, m, ) shaped array
+        
+        The second fuzzy matrix.
+    
+    t_norm : function
+
+        T-norm function for calculating the intersection.
+
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns intersection of the input matrices.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> B = T1FS(domain, mf=gaussian_mf, params=[0.4, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    >>> t1fmatrix2 = T1FMatrix(matrix, B)
+    >>> t1fmatrix3 = T1FMatrix_Intersection(t1fmatrix1, t1fmatrix2, min_t_norm)
+    """
     return t_norm(m1, m2)
 
 def T1FMatrix_Union(m1, m2, s_norm):
+    """
+    Calculating union of two type 1 fuzzy matrices.
+    
+    .. rubric:: Parameters
+    
+    m1 : numpy (n, m, ) shaped array
+        
+        The first fuzzy matrix.
+    
+    m2 : numpy (n, m, ) shaped array
+        
+        The second fuzzy matrix.
+    
+    s_norm : function
+
+        S-norm function for calculating the union.
+
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns union of the input matrices.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> B = T1FS(domain, mf=gaussian_mf, params=[0.4, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    >>> t1fmatrix2 = T1FMatrix(matrix, B)
+    >>> t1fmatrix3 = T1FMatrix_Union(t1fmatrix1, t1fmatrix2, max_s_norm)
+    """
     return s_norm(m1, m2)
 
 def Minmax(m1, m2):
+    """
+    Calculating min-max of two type 1 fuzzy matrices.
+    
+    .. rubric:: Parameters
+    
+    m1 : numpy (n, m, ) shaped array
+        
+        The first fuzzy matrix.
+    
+    m2 : numpy (n, m, ) shaped array
+        
+        The second fuzzy matrix.
+    
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns min-max of the input matrices.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> B = T1FS(domain, mf=gaussian_mf, params=[0.4, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    >>> t1fmatrix2 = T1FMatrix(matrix, B)
+    >>> t1fmatrix3 = Minmax(t1fmatrix1, t1fmatrix2)
+    """
     m, n = m1.shape
     r, q = m2.shape
     if r != n:
-        raise TypeError("Matrices are inconsistent.")
+        raise ValueError("Matrices are inconsistent.")
     o = zeros(shape=(m, q))
     for i in range(m):
         for j in range(q):
@@ -34,10 +178,39 @@ def Minmax(m1, m2):
     return o
 
 def Maxmin(m1, m2):
+    """
+    Calculating max-min of two type 1 fuzzy matrices.
+    
+    .. rubric:: Parameters
+    
+    m1 : numpy (n, m, ) shaped array
+        
+        The first fuzzy matrix.
+    
+    m2 : numpy (n, m, ) shaped array
+        
+        The second fuzzy matrix.
+    
+    .. rubric:: Returns
+    
+    output : numpy (n, m, ) shaped array
+        
+        Returns max-min of the input matrices.
+    
+    .. rubric:: Examples
+    
+    >>> matrix = random.rand(3, 3)
+    >>> domain = linspace(0., 1., 101)
+    >>> A = T1FS(domain, mf=gaussian_mf, params=[0.6, 0.2, 1.])
+    >>> B = T1FS(domain, mf=gaussian_mf, params=[0.4, 0.2, 1.])
+    >>> t1fmatrix1 = T1FMatrix(matrix, A)
+    >>> t1fmatrix2 = T1FMatrix(matrix, B)
+    >>> t1fmatrix3 = Maxmin(t1fmatrix1, t1fmatrix2)
+    """
     m, n = m1.shape
     r, q = m2.shape
     if r != n:
-        raise TypeError("Matrices are inconsistent.")
+        raise ValueError("Matrices are inconsistent.")
     o = zeros(shape=(m, q))
     for i in range(m):
         for j in range(q):
@@ -46,12 +219,47 @@ def Maxmin(m1, m2):
 
 
 def T1FMatrix_isNull(matrix):
+    """
+    Checks if all elements of the input matrix is zero.
+    
+    .. rubric:: Parameters
+    
+    matrix : numpy (n, m, ) shaped array
+        
+        The input fuzzy matrix.
+    
+    .. rubric:: Returns
+    
+    output : boolean
+        
+        Returns True if all elements are zero, else False.
+
+    """
     return array_equal(matrix, 0.)
 
 def T1FMatrix_isUniversal(matrix):
+    """
+    Checks if all elements of the input matrix is one.
+    
+    .. rubric:: Parameters
+    
+    matrix : numpy (n, m, ) shaped array
+        
+        The input fuzzy matrix.
+    
+    .. rubric:: Returns
+    
+    output : boolean
+        
+        Returns True if all elements are one, else False.
+
+    """
     return array_equal(matrix, 1.)
 
 def T1FSoftMatrix_Product(r, c, norm, *matrices):
+    """
+    Calculates soft matrix product.
+    """
     if len(matrices) == 0:
         raise ValueError("At least one matrix is required.")
     o = matrices[0]
@@ -61,6 +269,9 @@ def T1FSoftMatrix_Product(r, c, norm, *matrices):
 
 
 def T1FSoftMatrix(U, F):
+    """
+    Creates a soft matrix.
+    """
     U = array(U)
     t1fsoftmatrix = []
     for f in F:
